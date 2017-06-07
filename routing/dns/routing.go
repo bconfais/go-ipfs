4d58a79dd205798ea7d22f2ea24120a4839da56f
bconfais <b@confais.org>
2017-06-07 11:19:14 +0200

    Iterate over DNS

--- routing/dns/routing.go
+++ routing/dns/routing.go
@@ -1,6 +1,8 @@
 package dnsrouting
 
 import (
+  "fmt"
+  "net"
   config "github.com/ipfs/go-ipfs/repo/config"
   context "gx/ipfs/QmZy2y8t9zQH2a1b8q2ZSLKp17ATuJoCNxxyMFG5qFExpt/go-net/context"
   dns "github.com/miekg/dns"
@@ -20,6 +22,8 @@ type DNSClient struct {
   peerstore  pstore.Peerstore
   datastore  repo.Datastore
   ctx  context.Context
+
+  resolver string
 }
 
 func ConstructDNSRouting(ctx context.Context, h p2phost.Host, d repo.Datastore) (routing.IpfsRouting, error) {
@@ -30,6 +34,11 @@ func ConstructDNSRouting(ctx context.Context, h p2phost.Host, d repo.Datastore) 
   client.host = h
   client.datastore = d
   client.ctx = ctx
+
+  if "" != Cfg.DNS.Resolver {
+    client.resolver = Cfg.DNS.Resolver
+  }
+
   return client, nil
 }
 
@@ -72,10 +81,49 @@ func (c *DNSClient) PutValue(ctx context.Context, k key.Key, value []byte) error
 
 func (c *DNSClient) FindProvidersAsync_(ctx context.Context, k key.Key, out chan pstore.PeerInfo) error {
   log.Debugf("FindProvidersAsync_")
-  c := new(dns.Client)
-  m := new(dns.Msg)
-  
+  client := new(dns.Client)
+  message := new(dns.Msg)
+  message.SetQuestion(dns.Fqdn(string(k)), dns.TypeNS)
+  message.RecursionDesired = false
+
+
+  var authority_servers []string // a stack of partial answers
+  authority_servers = append(authority_servers, c.resolver) 
+
+  for 0 != len(authority_servers) {
+    authority_server := (authority_servers)[len(authority_servers)-1]
+    authority_servers = (authority_servers)[0:len(authority_servers)-1]
+
+    r, _, err := client.Exchange(message, net.JoinHostPort(authority_server, "53"))
+    if r == nil {
+      log.Fatalf("**** error: %s\n", err.Error())
+      log.Debugf("**** We need to try another DNS server\n")
+      continue
+    }
+    if r.Rcode != dns.RcodeSuccess {
+      log.Fatalf("**** invalid answer name %s after query for %s\n", string(k), string(k))
+      log.Fatalf("**** Object does not exist, perhaps try another path in the DNS\n");
+      continue
+    }
+    if 0 == len(r.Extra) {
+      log.Fatalf("**** Lookup stalled\n, try another path\n")
+      continue
+    }
+/*
+    for _, a := range r.Ns {
+      fmt.Printf("%v\n", a)
+    }
+*/
+    for _, a := range r.Extra {
+      next_server := string(a.(*dns.A).A)
+      authority_servers = append(authority_servers, next_server)
+      fmt.Printf("%v\n", next_server)
+    }
+
+  }
+  ctx.Done()
   return nil
+
 }
 
 var Cfg *config.Config
